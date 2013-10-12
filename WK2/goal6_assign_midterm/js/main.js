d086b865f69645258dd8778e217a5b257f4b3dc2
Joshua Velez <Joshv1288@gmail.com>
2013-10-12 01:46:15 -1000

    Updated code

--- WK2/goal6_assign_midterm/js/main.js
+++ WK2/goal6_assign_midterm/js/main.js
@@ -1,60 +1,55 @@
--/*
+/*
  -	* Mid Terms for PWA-1
- -*/
+ Joshua Velez
+*/
+(function(){
+
 
 var fsInfo1 = {
     name: 'Joshua Velez',
-    address:{
+    address: {
         street: '94-1411 Kulewa Loop ',
         city: 'Waipahu, ',
         state: 'HI'
     },
-    gpa: [3.3 ,4.0 ,2.8]
+    gpa: [3.3 , 4.0 , 2.8]
 
-}
+};
 var fsInfo2= {
     name: 'John Doe',
     address:{
-        street: 'Some Street ',
+        street: '123 Some Street ',
         city: 'Wahiawa, ',
         state: 'HI'
     },
     gpa: [3.4, 3.6, 3.1]
-}
-
-
-var info = new Object();
-
-info[0] = fsInfo1;
-info[1] = fsInfo2;
-var count = Object.keys(info).length;
-for(i=0; i<count;i++){
-    console.log("Name: " + info[i].name)
-    console.log("Address: " + info[i].address['street'] + info[i].address['city'] + info[i].address['state'])
-    console.log("GPA: " + info[i].gpa[0],info[i].gpa[1],info[i].gpa[2])
-
-    function startUp(){
-        document.getElementById("name").innerHTML = "Name: " + info[0].name
-        document.getElementById("address").innerHTML = "Address: " + info[0].address['street'] + info[0].address['city'] + info[0].address['state']
-        document.getElementById("gpa").innerHTML = "GPA: " + info[0].gpa
-        document.getElementById("date").innerHTML = "Date: " + date()
-        document.getElementById("gpaavg").innerHTML = "Average GPA: " + calc()
-
-    }
-}
-
-function calc(){
-var Sum = 0;
-for(x=0; x<count; x++){
-    Sum = Sum + info[x].gpa[x];
-}
-    average = Sum / count;
-    return average
 };
 
-function date(){
+
+var info = {};
+info[0] = fsInfo1;
+info[1] = fsInfo2;
+
+var count = Object.keys(info).length;
+for(var i=0; i<count;i++){
+    console.log("Name: " + info[i].name);
+    console.log("Address: " + info[i].address.street + info[i].address.city + info[i].address.state);
+    console.log("GPA: " + info[i].gpa[0],info[i].gpa[1],info[i].gpa[2]);
+    console.log("Date: " + date());
+}
+    window.onload = function startup() {
+        document.getElementById("name").innerHTML = "Name: " + info[0].name;
+        document.getElementById("address").innerHTML = "Address: " + info[0].address.street + info[0].address.city + info[0].address.state;
+        document.getElementById("gpa").innerHTML = "GPA: " + info[0].gpa;
+        document.getElementById("date").innerHTML = "Date: " + date();
+        document.getElementById("gpaavg").innerHTML = "Average GPA: " + " " ;
+        return false;
+
+    };
+
+    function date(){
     var d = new Date();
-    var month=new Array();
+    var month=[];
     month[0]="January";
     month[1]="February";
     month[2]="March";
@@ -67,14 +62,17 @@ function date(){
     month[9]="October";
     month[10]="November";
     month[11]="December";
-    var n = month[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
-    return n
+    return month[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
 }
 
-document.getElementById("redbutton").innerHTML.onclick = function myFunction(e){
+addData('Jane Smith', '42 Wallaby Way', 'Aiea', 'Hawaii', [3.2, 4.0, 2.2] )
 
 
-    e.preventDefault();
-    return false;
-    };
-
+    function addInfo(){
+        document.getElementById("name").innerHTML = "Name: " + info[1].name;
+        document.getElementById("address").innerHTML = "Address: " + info[1].address.street + info[1].address.city + info[1].address.state;
+        document.getElementById("gpa").innerHTML = "GPA: " + info[1].gpa;
+        document.getElementById("date").innerHTML = "Date: " + date();
+        document.getElementById("gpaavg").innerHTML = "Average GPA: " + " "
+    }
+})();
