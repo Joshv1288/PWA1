9230d90c6954b86ed64c8d1cbfd7e6452d68430a
Joshua Velez <Joshv1288@gmail.com>
2013-10-11 17:06:44 -1000

    removed function, change var name

--- WK2/goal6_assign_midterm/js/main.js
+++ WK2/goal6_assign_midterm/js/main.js
@@ -2,19 +2,20 @@
 	* Mid Terms for PWA-1
 */
 
-(function(){
-  
- var student = {
- 	name: ‘Joshua Velez’, 
- 	address:{
- 		address: ‘94-1411 Kulewa Loop’, 
- 		city: ‘Waipahu’, 
- 		state: ‘HI’}, 
- 	gpa: [2.5, 3.5, 4.0]}
-  
-  console.log(student.name)
-  console.log(student.['address'])
-  console.log(student.[gpa])
-  
-  
-})();
+
+
+var fsInfo = {
+    name: 'Joshua Velez',
+    address:{
+        street: '94-1411 Kulewa Loop',
+        city: 'Waipahu',
+        state: 'HI',
+    gpa: [3.0,4.0,2.0]
+    }
+};
+
+avgGPA = fsInfo.gpa[1], fsInfo.gpa[2], fsInfo.gpa [3]
+
+console.log("Name: " + fsInfo.name)
+console.log("Address: " + fsInfo['address'])
+console.log("GPA: " + avgGPA)
